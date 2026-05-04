95b27a33344e14d934cbeb41bc81c70746577d2b
tanzorr <alexx1984@ukr.net>
2026-05-04 10:35:25 +0300

    add/form task

--- src/index.js
+++ src/index.js
@@ -1,21 +1,35 @@
-import { taskStoreArray } from './data.js';
+import {taskStoreArray} from './data.js';
 
-import { PROMPTS } from './constants.js';
-import { taskValidation } from './validations.js';
-import { renderTodoList } from './render.js';
+import {PROMPTS} from './constants.js';
+import {taskValidation} from './validations.js';
+import {renderTodoList} from './render.js';
 
 const addButton = document.querySelector('.add-task-btn');
 
 renderTodoList();
 
-addButton.onclick = addNewTodoToArray;
+//addButton.onclick = addNewTodoToArray;
 
-function addNewTodoToArray() {
-    //Get input from a user using a prompt
-    const inputValue = window.prompt(PROMPTS.addTask);
+// function addNewTodoToArray() {
+//     //Get input from a user using a prompt
+//     const inputValue = window.prompt(PROMPTS.addTask);
+//
+//     if (taskValidation(inputValue, taskStoreArray)) {
+//         taskStoreArray.push(inputValue);
+//         renderTodoList();
+//     }
+// }
 
-    if (taskValidation(inputValue, taskStoreArray)) {
-        taskStoreArray.push(inputValue);
+
+const form = document.querySelector('.form');
+const taskInput = form.querySelector('.input-task');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if (taskValidation(taskInput.value, taskStoreArray)) {
+        taskStoreArray.push(taskInput.value);
         renderTodoList();
+        taskInput.value = '';
     }
-}
+});
