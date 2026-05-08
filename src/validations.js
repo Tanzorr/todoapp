9b4fcac5a160b1b3352e81b47dff4562c5034d14
tanzorr <alexx1984@ukr.net>
2026-05-08 09:42:50 +0300

    add/form display error validation

--- src/validations.js
+++ src/validations.js
@@ -1,22 +1,29 @@
 import { MIN_TASK_LENGTH, MAX_TASK_LENGTH, ALERTS } from './constants.js';
 
+const errorContainer = document.querySelector('.error-container');
+
+export function displayError(message) {
+    errorContainer.textContent = message;
+}
+
 export function taskValidation(taskString, taskStoreArray) {
     switch (true) {
         case taskString === null:
             return false;
         case taskStoreArray.includes(taskString):
-            alert(ALERTS.taskExists);
+            displayError(ALERTS.taskExists);
             return false;
         case taskString.trim() === '':
-            alert(ALERTS.emptyTask);
+            displayError(ALERTS.emptyTask);
             return false;
         case taskString.length < MIN_TASK_LENGTH:
-            alert(ALERTS.minLength);
+            displayError(ALERTS.minLength);
             return false;
         case taskString.length > MAX_TASK_LENGTH:
-            alert(ALERTS.maxLength);
+            displayError(ALERTS.maxLength);
             return false;
         default:
+            displayError('');
             return true;
     }
 }
